55a8ae68424b1ce17cf00f08effab8cf9ed8278d
weezqyd <wizqydy@gmail.com>
2016-02-11 14:28:43 +0300

    major core updates

--- tests/Test.php
+++ tests/Test.php
@@ -1,28 +1,2 @@
 <?php
-namespace Sematime;
-
-require __DIR__.'/vendor/autoload.php';
-        use Sematime\Api\Sematime;
-        
-        
-        // An Array of recipients
-        $recipients=array('contactId'=>'1234567890', 'name'=>'Allan Kibet Kirui','phoneNumber'=>'0727038269');
-        // Initialize The Sematime Api
-        $sema = new Sematime();
-        $response=$sema->addGroup('members')->addId('1234567890')->addName('Allan Kibet Kirui')->addPhone('0727038269')->save();
-        //print json_encode($response->contact);
-        //print json_encode($sema->contact);
-        echo $response;
-
-
-        /*$message='nice one a message send through the awesome sematime api';
-            $results = $gateway->sendMessage($recipients, $message);
-            isset($results) ? $response = $results : $response = 'An Error Was Encoutered';
-            
-            echo $response ;
-        
-        $dotenv = new Dotenv(__DIR__);
-        $dotenv->load();
-        echo getenv('API_KEY');
-        var_dump($dotenv);
-        ?>*/
+curl_init();
